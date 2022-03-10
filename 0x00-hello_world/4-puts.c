85b659139137f5ad34eb6b30a19a0d45d196865f
davis-sang <davissang08@gmail.com>
2022-03-10 21:26:54 +0000

    task 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* initialize string*/
+	char str1[] = "Programming is like building a multilingual puzzle";
+
+	/*writing to stdout*/
+	puts(str1);
+
+	return (0);
+}
+
+
