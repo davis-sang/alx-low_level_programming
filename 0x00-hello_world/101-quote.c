15f9c52048b082df46f6e8b6e41942685d08d2af
davis-sang <davissang08@gmail.com>
2022-03-11 10:31:16 +0000

    advanced tasks 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  *
@@ -8,7 +9,9 @@
  */
 int main(void)
 {
-	const char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, x, strlen(x));
+int p;
+
+	p = strlen("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", p);
 	return (0);
 }
