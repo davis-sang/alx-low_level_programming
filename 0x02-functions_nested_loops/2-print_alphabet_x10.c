ede3793d175848c9e3e99a1a8e0432a87a89368c
davis-sang <davissang08@gmail.com>
2022-03-16 15:13:09 +0000

    task 3 print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints the alphabet in lower case followed by a new line.
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	i = 10;
+	while (i < 10)
+	{
+		char l;
+
+		l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		i++;
+	}
+}
