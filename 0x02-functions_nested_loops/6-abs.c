1123bcf03ed9f9fc5b72e65fb1db8406d97a8d9f
davis-sang <davissang08@gmail.com>
2022-03-16 22:33:08 +0000

    task 6 absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_abs - main function
+*@j: an integer input
+*Description: function returns the absolute vakue of a number
+*Return: absolute value of a number
+*/
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
