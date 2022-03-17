ffa30eeb83a209119a2d2860d394b3728d97ca1a
davis-sang <davissang08@gmail.com>
2022-03-17 21:25:04 +0000

    task 0 isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 /**
 *_isupper - main function
 *Description: checks if input is uppercase
-*@c - input character entered
+*@c:  input character entered
 *Return: return 0 if lowercase and 1 for uppercase
 */
 int _isupper(int c);
@@ -10,12 +10,12 @@ int _isupper(int c);
 	char l;
 	int i;
 
-	for(l = 'A'; l <= 'Z'; l++)
+	for (l = 'A'; l <= 'Z'; l++)
 	{
 		if (l == c)
 			i = 1;
 		else
 			i = 0;
 	}
-	return i;
+	return (i);
 }
