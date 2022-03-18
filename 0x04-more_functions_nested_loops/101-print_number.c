3716433485725535d5a894e8bc42854f4a00bda7
davis-sang <davissang08@gmail.com>
2022-03-18 22:31:33 +0000

    task 101 print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,9 +11,11 @@ void print_number(int n)
 		n = n * -1;
 		_putchar('-');
 	}
-	if (n == 0)
-		_putchar(0);
+	else
+	{
+		n = n;
+	}
 	if (n / 10)
 		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }
