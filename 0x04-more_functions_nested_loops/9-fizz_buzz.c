8f3b6f72db365b0d436e249f6d0135808716c4d4
davis-sang <davissang08@gmail.com>
2022-03-18 15:32:46 +0000

    Task 9 fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - main function
+*Description: fizz-buzz test, fizz for multiples of 3, buzz for 5 and *fizbuzz for both.
+*Return: 0 success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%i ",i);
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("Buzz ");
+	}
+	printf("\n");
+	return (0);
+}
