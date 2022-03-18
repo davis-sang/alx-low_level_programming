bb4a6e36aba47e52d45da894dbfeef51ef52f571
davis-sang <davissang08@gmail.com>
2022-03-18 22:17:22 +0000

    task 101 print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_number - print number entered
+*@n: integer entered
+*Return: none
+*/
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n == 0)
+		_putchar(0);
+	
+	if (n / 10)
+		print_number (n / 10);
+	_putchar(n % 10 + '0'); 
+}
