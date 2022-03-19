7d37b4ae4fe1f66cf47fc8a0143d68385ca0cfbb
davis-sang <davissang08@gmail.com>
2022-03-19 12:59:48 +0000

    advanced task 102 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main function
+*Description: prints the first 50 fibonacci numbers
+*Return: 0
+*/
+int main (void)
+{
+	int i, j, k, count;
+
+	i = 1;
+	j = 2;
+	k = 0;
+	count = 49;
+	for (i = 0;i < count; i++)
+	{
+		printf("%i ",i);
+		k = i + j;
+		i = j;
+		j = k;
+		if (i == 48)
+			printf("%i\n", i);
+	}
+	return (0);
+} 
