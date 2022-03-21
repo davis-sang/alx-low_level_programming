2ab3a38624b2eb4a534576ce20853bf8a1e4ad12
davis-sang <davissang08@gmail.com>
2022-03-21 22:22:10 +0000

    task 4 print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_rev - main function
+*@s: input string
+*Description: prints string in reverse
+*Return: none
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
