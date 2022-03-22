13ce8907d000ce56f5ef7f6c45fbbb8edf42c339
davis-sang <davissang08@gmail.com>
2022-03-22 20:20:02 +0000

    task 9 copies string src to dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,8 @@ char *_strcpy(char *dest, char *src)
 	for (; c >= 0; c++)
 	{
 		*(dest + c) = *(src + c);
-		if(*(src + c) == '\0')
+		if (*(src + c) == '\0')
 			break;
 	}
 	return (dest);
 }
-	
