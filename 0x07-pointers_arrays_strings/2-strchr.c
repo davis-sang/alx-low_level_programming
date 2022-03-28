f83241af27b1e7c31f5eeac4ab432e358e4bd784
davis-sang <davissang08@gmail.com>
2022-03-29 00:37:41 +0300

    task 2 strchr implementation

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,14 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int i;
 
-	while (*s != '\0')
-	{
-		if (*(s + i) == c)
-			return (s + i);
-		i++;
-	}
-	if (*(s + i) == c)
-		return (s + i);
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
+
+	if (s[i] == c)
+		return (&s[i]);
+
 	return ('\0');
 }
