9809a272beb1130ab6f7b3509a6a834ee0281f5c
davis-sang <davissang08@gmail.com>
2022-04-03 10:37:09 +0300

    Task 0 prints program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,13 @@
 /**
 *main - main function
 *Description: program print the program name followed by a new lline.
+*@argc: argument counter.
+*@argv: used to refer to an array of strings.
 *Return: 0
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
