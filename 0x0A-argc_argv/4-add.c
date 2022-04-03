e33a4c9b29c9b0675fe0e915d76e4d3ab80799d4
davis-sang <davissang08@gmail.com>
2022-04-03 13:21:19 +0300

    Task 4 add integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main function
+*@argc: argument counter
+*@argv: array of strings
+*Description: Adds positive numbers.
+*Return: 0 on success.
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
