9cfd560cdf921e6b901055375e89557365c8ff0b
davis-sang <davissang08@gmail.com>
2022-04-04 23:07:33 +0300

    Task 2 : string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,10 +11,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *s;
 	char *p;
+	char *c = "";
 	int i, j;
 
-	if (s1 == NULL && s2 == NULL)
-		return (0);
+	if (s1 == NULL)
+		s1 = c;
+	if (s2 == NULL)
+		s2 = c;
 	i = 0;
 	j = 0;
 	while (s1[i])
