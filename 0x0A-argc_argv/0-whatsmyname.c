4b565ae0ed268bdad509197ac2db769b13192c94
davis-sang <davissang08@gmail.com>
2022-04-04 12:07:38 +0300

    Task 0 : print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
 *@argv: used to refer to an array of strings.
 *Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
