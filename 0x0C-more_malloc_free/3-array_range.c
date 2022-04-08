b23363ab9d0c85165118e771225df94dc2d89686
davis-sang <davissang08@gmail.com>
2022-04-08 20:52:30 +0300

    Task 3: function crates an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,10 +10,10 @@ int *array_range(int min, int max)
 {
 	int *a;
 	int i, j;
-	
+
 	if (min > max)
 		return (NULL);
-	j = max -min + 1;
+	j = max - min + 1;
 	a = malloc(sizeof(int) * j);
 	if (a == NULL)
 		return (NULL);
