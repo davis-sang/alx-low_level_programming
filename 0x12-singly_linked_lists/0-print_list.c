3b2197c687adc5cd59a9c80b0e0236a34f7b30ad
Davis-sang <davissang08@gmail.com>
2022-04-24 18:43:26 +0300

    Task 0: print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t l
+	size_t size = 0;
 
-	l = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+		size++;
 		h = h->next;
-		list++;
 	}
-	return (i);
+	return (size);
 }
